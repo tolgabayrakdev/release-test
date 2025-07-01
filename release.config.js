cebc874fda108b3809ba72e27b819e18ac822c43
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-07-01 10:10:55 +0300

    feat: inital commit

---
+++ release.config.js
@@ -0,0 +1,15 @@
+export default {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
